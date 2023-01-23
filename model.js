a042143ebd13fa8495e5e86f7bfd2c88d8192f1f
Georgia E. Whitney <81175151+rockyrelay@users.noreply.github.com>
2023-01-23 13:00:35 +0000

    test passed - 5

--- model.js
+++ model.js
@@ -26,25 +26,26 @@ function listStudentsWithLocation() {
 }
 
 const select_students_with_projects = db.prepare(/*sql*/ `
-
+  SELECT projects.name, students.username 
+  FROM projects JOIN students_projects ON projects.id = students_projects.project_id JOIN students ON students.username = students_projects.student_username  
 `);
 
 function listStudentsWithProjects() {
   return select_students_with_projects.all();
 }
 
-// const select_students_with_projects_in_finsbo = db.prepare(/*sql*/ `
-//   -- [5]
-// `);
+const select_students_with_projects_in_finsbo = db.prepare(/*sql*/ `
+  SELECT projects.name, students.username FROM projects JOIN students_projects ON projects.id = students_projects.project_id JOIN students ON students.username = students_projects.student_username WHERE students.username IN ( SELECT username FROM students WHERE cohort_name IN ( SELECT name FROM cohorts WHERE location = 'Finsbury Park')) 
+`);
 
-// function listStudentsWithProjectsInFinsbo() {
-//   return select_students_with_projects_in_finsbo.all();
-// }
+function listStudentsWithProjectsInFinsbo() {
+  return select_students_with_projects_in_finsbo.all();
+}
 
 module.exports = {
   listCohortsInFinsbo,
   listStudentsInFinsbo,
   listStudentsWithLocation,
   listStudentsWithProjects,
-  // listStudentsWithProjectsInFinsbo,
+  listStudentsWithProjectsInFinsbo,
 };
